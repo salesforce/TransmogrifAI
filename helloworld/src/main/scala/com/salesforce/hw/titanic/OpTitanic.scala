ddb68dc368b2524bfe9d4cc8b47ee1bfce3116ad
Matthew Tovbin <mtovbin@salesforce.com>
2018-05-27 18:50:18 -0700

    Update license + simplify Titanic example

--- helloworld/src/main/scala/com/salesforce/hw/titanic/OpTitanic.scala
+++ helloworld/src/main/scala/com/salesforce/hw/titanic/OpTitanic.scala
@@ -1,111 +1,110 @@
 /*
  * Copyright (c) 2017, Salesforce.com, Inc.
  * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Salesforce.com nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.salesforce.hw.titanic
 
 import com.salesforce.op._
+import com.salesforce.op.features.types._
+import com.salesforce.op.features._
 import com.salesforce.op.evaluators.Evaluators
-import com.salesforce.op.readers.CSVReader
+import com.salesforce.op.readers.DataReaders
 import com.salesforce.op.stages.impl.classification.ClassificationModelsToTry._
 import com.salesforce.op.stages.impl.classification._
 import com.salesforce.op.stages.impl.preparators.SanityChecker
 import com.salesforce.op.stages.impl.tuning.DataSplitter
 import com.salesforce.op.utils.kryo.OpKryoRegistrator
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Dataset, SparkSession}
 
 /**
  * Optimus Prime example classification app using the Titanic dataset
  */
 object OpTitanic extends OpAppWithRunner with TitanicFeatures {
 
-
   override def kryoRegistrator: Class[_ <: OpKryoRegistrator] = classOf[TitanicKryoRegistrator]
 
-
   ////////////////////////////////////////////////////////////////////////////////
-  // READER DEFINITIONS
+  // READER DEFINITION
   /////////////////////////////////////////////////////////////////////////////////
 
   val randomSeed = 112233
-
-  val trainingReader = new CSVReader[Passenger](
-    readPath = None,
-    schema = Passenger.getClassSchema.toString,
-    key = _.getPassengerId.toString // entity to score
-  ) {
-    override def read(params: OpParams)
-      (implicit sc: SparkSession): Either[RDD[Passenger], Dataset[Passenger]] = {
-      val data = super.read(params)
-      data match {
-        case Left(dataRDD) =>
-          val Array(train, _) = dataRDD.randomSplit(weights = Array(0.9, 0.1), seed = randomSeed)
-          Left(train)
-        case Right(dataSet) =>
-          val Array(train, _) = dataSet.randomSplit(weights = Array(0.9, 0.1), seed = randomSeed)
-          Right(train)
-      }
-    }
-  }
-
-  // Note not using the titanic test data because it has no labels so cannot evaluate
-  val scoringReader = new CSVReader[Passenger](
-    readPath = None,
-    schema = Passenger.getClassSchema.toString,
-    key = _.getPassengerId.toString // entity to score
-  ) {
-    override def read(params: OpParams)
-      (implicit sc: SparkSession): Either[RDD[Passenger], Dataset[Passenger]] = {
-      val data = super.read(params)
-      data match {
-        case Left(dataRDD) =>
-          val Array(_, test) = dataRDD.randomSplit(weights = Array(0.9, 0.1), seed = randomSeed)
-          Left(test)
-        case Right(dataSet) =>
-          val Array(_, test) = dataSet.randomSplit(weights = Array(0.9, 0.1), seed = randomSeed)
-          Right(test)
-      }
-    }
-  }
+  val simpleReader = DataReaders.Simple.csv[Passenger](
+    path = None, schema = Passenger.getClassSchema.toString,
+    key = _.getPassengerId.toString
+  )
 
   ////////////////////////////////////////////////////////////////////////////////
   // WORKFLOW DEFINITION
   /////////////////////////////////////////////////////////////////////////////////
 
-  val featureVector = Seq(pClass, name, sex, age, sibSp, parch, ticket, cabin, embarked).transmogrify()
+  // val namePick = name.map[PickList](_.value.toPickList)
+
+  val featureVector = Seq(
+    pClass, name, sex, age, sibSp,
+    parch, ticket, cabin, embarked
+  ).transmogrify()
 
   val checkedFeatures = new SanityChecker()
-    .setCheckSample(0.10)
+    .setCheckSample(1.0)
     .setSampleSeed(randomSeed)
     .setInput(survived, featureVector)
+    .setRemoveBadFeatures(true)
     .getOutput()
 
+  val splitter = DataSplitter(seed = randomSeed, reserveTestFraction = 0.1)
 
-  val (pred, raw, prob) = BinaryClassificationModelSelector
-    .withCrossValidation(splitter = Option(DataSplitter(seed = randomSeed)), seed = randomSeed)
-    .setLogisticRegressionRegParam(0.05, 0.1)
-    .setLogisticRegressionElasticNetParam(0.01)
-    .setRandomForestMaxDepth(5, 10)
-    .setRandomForestMinInstancesPerNode(10, 20, 30)
-    .setRandomForestSeed(randomSeed)
-    .setModelsToTry(LogisticRegression, RandomForest)
-    .setInput(survived, checkedFeatures)
-    .getOutput()
+  val (pred, raw, prob) =
+    BinaryClassificationModelSelector
+      .withCrossValidation(splitter = Option(splitter), seed = randomSeed)
+      .setLogisticRegressionRegParam(0.05, 0.1)
+      .setLogisticRegressionElasticNetParam(0.01)
+      .setRandomForestMaxDepth(5, 10)
+      .setRandomForestMinInstancesPerNode(10, 20, 30)
+      .setRandomForestSeed(randomSeed)
+      .setModelsToTry(LogisticRegression, RandomForest)
+      .setInput(survived, checkedFeatures)
+      .getOutput()
 
   val workflow = new OpWorkflow().setResultFeatures(pred, raw)
 
   val evaluator =
     Evaluators.BinaryClassification()
-      .setLabelCol(survived).setPredictionCol(pred).setRawPredictionCol(raw)
+      .setLabelCol(survived)
+      .setPredictionCol(pred)
+      .setRawPredictionCol(raw)
 
   def runner(opParams: OpParams): OpWorkflowRunner =
     new OpWorkflowRunner(
       workflow = workflow,
-      trainingReader = trainingReader,
-      scoringReader = scoringReader,
-      evaluationReader = Option(trainingReader),
+      trainingReader = simpleReader,
+      scoringReader = simpleReader,
+      evaluationReader = Option(simpleReader),
       evaluator = Option(evaluator),
       featureToComputeUpTo = Option(featureVector)
     )
