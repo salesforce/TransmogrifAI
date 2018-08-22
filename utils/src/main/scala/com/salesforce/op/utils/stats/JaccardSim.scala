b9d5c31debae621500d5174c7957e3c60db94528
Till Bergmann <tbergmann@ucmerced.edu>
2018-08-22 15:44:05 -0700

    Renaming JaccardDistance to JaccardSimilarity (#80)

--- utils/src/main/scala/com/salesforce/op/utils/stats/JaccardSim.scala
+++ utils/src/main/scala/com/salesforce/op/utils/stats/JaccardSim.scala
@@ -31,19 +31,19 @@
 package com.salesforce.op.utils.stats
 
 /**
- * Calculates the Jaccard Distance between two sets.
- * If both inputs are empty, Jaccard Distance is defined as 1.0
+ * Calculates the Jaccard Similarity between two sets.
+ * If both inputs are empty, Jaccard Similarity is defined as 1.0
  */
-object JaccardDistance {
+object JaccardSim {
 
   /**
-   * Calculates the Jaccard Distance between two sets.
-   * If both inputs are empty, Jaccard Distance is defined as 1.0
+   * Calculates the Jaccard Similarity between two sets.
+   * If both inputs are empty, Jaccard Similarity is defined as 1.0
    *
    * @param s1 first set
    * @param s2 second set
    * @tparam A set type
-   * @return Jaccard Distance
+   * @return Jaccard Similarity
    */
   def apply[A](s1: Set[A], s2: Set[A]): Double = {
     val intersectSize = s1.intersect(s2).size
