9e9a05c239d30989079843c712d6ad06452116ff
Matthew Tovbin <mtovbin@salesforce.com>
2018-08-22 12:19:54 -0700

    Fixes

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ html_show_sphinx = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md', 'Makefile']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
