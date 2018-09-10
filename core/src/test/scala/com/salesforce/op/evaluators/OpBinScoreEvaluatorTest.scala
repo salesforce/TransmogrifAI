b421d33199c78e462fa18c8e3430bd6900c2ffb4
Gokulakrishnan <7522696+gokulsfdc@users.noreply.github.com>
2018-09-10 23:18:03 +0530

    fixing testbuilder for tests

--- core/src/test/scala/com/salesforce/op/evaluators/OpBinScoreEvaluatorTest.scala
+++ core/src/test/scala/com/salesforce/op/evaluators/OpBinScoreEvaluatorTest.scala
@@ -31,41 +31,41 @@
 package com.salesforce.op.evaluators
 
 import com.salesforce.op.features.types.Prediction
-import com.salesforce.op.test.TestSparkContext
+import com.salesforce.op.features.types._
+import com.salesforce.op.test.{TestFeatureBuilder, TestSparkContext}
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
+
 @RunWith(classOf[JUnitRunner])
 class OpBinScoreEvaluatorTest extends FlatSpec with TestSparkContext {
 
-  val labelName = "label"
-  val predictionLabel = "pred"
+  val (dataset, prediction, label) = TestFeatureBuilder(
+    Seq (
+      Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)) -> 1.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)) -> 1.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.99560, 0.00541)) -> 0.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.30, 0.70)) -> 0.0.toRealNN,
+      Prediction(0.0, Array(10.0, 10.0), Array(0.999, 0.001)) -> 0.0.toRealNN
+    )
+  )
 
-  val dataset_test = Seq(
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)), 1.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)), 1.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.99560, 0.00541)), 0.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.30, 0.70)), 0.0),
-    (Prediction(0.0, Array(10.0, 10.0), Array(0.999, 0.001)), 0.0)
-  ).map(v => (v._1.toDoubleMap, v._2))
+  val (dataset_skewed, prediction_skewed, label_skewed) = TestFeatureBuilder(
+    Seq (
+      Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)) -> 1.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)) -> 1.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.001, 0.9987)) -> 1.0.toRealNN,
+      Prediction(1.0, Array(10.0, 10.0), Array(0.0541, 0.946)) -> 1.0.toRealNN
+    )
+  )
 
-  val dataset_test_df = spark.createDataFrame(dataset_test).toDF(predictionLabel, labelName)
-
-  val dataset_skewed = Seq(
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)), 1.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.0001, 0.99999)), 1.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.001, 0.9987)), 1.0),
-    (Prediction(1.0, Array(10.0, 10.0), Array(0.0541, 0.946)), 1.0)
-  ).map(v => (v._1.toDoubleMap, v._2))
-
-  val dataset_skewed_df = spark.createDataFrame(dataset_skewed).toDF(predictionLabel, labelName)
-
-  val emptyDataSet_df = spark.createDataFrame(Seq.empty[(Map[String, Double], Double)])
-    .toDF(predictionLabel, labelName)
+  val (emptyDataSet, prediction_emptydataset, label_empty_dataset) = TestFeatureBuilder[Prediction, RealNN](
+    Seq()
+  )
 
   Spec[OpBinScoreEvaluator] should "return the bin metrics" in {
     val metrics = new OpBinScoreEvaluator(numBins = 4)
-      .setLabelCol(labelName).setPredictionCol(predictionLabel).evaluateAll(dataset_test_df)
+          .setLabelCol(label.name).setPredictionCol(prediction.name).evaluateAll(dataset)
 
     metrics shouldBe BinaryClassificationBinMetrics(
       0.09800605366,
@@ -78,20 +78,20 @@ class OpBinScoreEvaluatorTest extends FlatSpec with TestSparkContext {
   it should "error on invalid number of bins" in {
     assertThrows[IllegalArgumentException] {
       new OpBinScoreEvaluator(numBins = 0)
-        .setLabelCol(labelName).setPredictionCol(predictionLabel).evaluateAll(dataset_test_df)
+        .setLabelCol(label.name).setPredictionCol(prediction.name).evaluateAll(dataset)
     }
   }
 
   it should "evaluate the empty data" in {
     val metrics = new OpBinScoreEvaluator(numBins = 10)
-      .setLabelCol(labelName).setPredictionCol(predictionLabel).evaluateAll(emptyDataSet_df)
+      .setLabelCol(label_empty_dataset.name).setPredictionCol(prediction_emptydataset.name).evaluateAll(emptyDataSet)
 
     metrics shouldBe BinaryClassificationBinMetrics(0.0, Seq(), Seq(), Seq(), Seq())
   }
 
   it should "evaluate bin metrics for skewed data" in {
     val metrics = new OpBinScoreEvaluator(numBins = 5)
-      .setLabelCol(labelName).setPredictionCol(predictionLabel).evaluateAll(dataset_skewed_df)
+      .setLabelCol(label_skewed.name).setPredictionCol(prediction_skewed.name).evaluateAll(dataset_skewed)
 
     metrics shouldBe BinaryClassificationBinMetrics(
       7.294225500000013E-4,
@@ -103,9 +103,8 @@ class OpBinScoreEvaluatorTest extends FlatSpec with TestSparkContext {
 
   it should "evaluate the default metric as BrierScore" in {
     val evaluator = new OpBinScoreEvaluator(numBins = 4)
-      .setLabelCol(labelName).setPredictionCol(predictionLabel)
+      .setLabelCol(label.name).setPredictionCol(prediction.name)
 
-    val brierScore = evaluator.getDefaultMetric(evaluator.evaluateAll(dataset_test_df))
-    BigDecimal(brierScore).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble shouldBe 0.098
+    evaluator.getDefaultMetric(evaluator.evaluateAll(dataset)) shouldBe 0.09800605366
   }
 }
