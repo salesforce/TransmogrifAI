1d0ae598a4da112eff589a10873d7e0052692241
hmittal <14928164+himsmittal@users.noreply.github.com>
2018-10-05 16:30:43 +0530

    Test Changes

--- core/src/test/scala/com/salesforce/op/ModelInsightsTest.scala
+++ core/src/test/scala/com/salesforce/op/ModelInsightsTest.scala
@@ -161,7 +161,7 @@ class ModelInsightsTest extends FlatSpec with PassengerSparkFixtureTest {
     insights.label.rawFeatureName shouldBe Seq(survived.name)
     insights.label.rawFeatureType shouldBe Seq(survived.typeName)
     insights.label.stagesApplied.size shouldBe 1
-    insights.label.sampleSize shouldBe Some(4.0)
+    insights.label.sampleSize shouldBe Some(5.0)
     insights.features.size shouldBe 5
     insights.features.map(_.featureName).toSet shouldEqual rawNames
     ageInsights.derivedFeatures.size shouldBe 2
@@ -213,12 +213,13 @@ class ModelInsightsTest extends FlatSpec with PassengerSparkFixtureTest {
     insights.label.rawFeatureName shouldBe Seq(survived.name)
     insights.label.rawFeatureType shouldBe Seq(survived.typeName)
     insights.label.stagesApplied.size shouldBe 1
-    insights.label.sampleSize shouldBe Some(4.0)
+    insights.label.sampleSize shouldBe Some(5.0)
     insights.features.size shouldBe 5
     insights.features.map(_.featureName).toSet shouldEqual rawNames
     ageInsights.derivedFeatures.size shouldBe 2
+    ageInsights.derivedFeatures(0).contribution.size shouldBe 1
+    ageInsights.derivedFeatures(1).contribution.size shouldBe 0
     ageInsights.derivedFeatures.foreach { f =>
-      f.contribution.size shouldBe 1
       f.corr.nonEmpty shouldBe true
       f.variance.nonEmpty shouldBe true
       f.cramersV.isEmpty shouldBe true
@@ -304,11 +305,10 @@ class ModelInsightsTest extends FlatSpec with PassengerSparkFixtureTest {
         pretty should not include(m.modelName)
       }
     }
-    pretty should include("area under precision-recall | 0.0")
+    pretty should include("area under precision-recall | 1.0")
     pretty should include("Model Evaluation Metrics")
     pretty should include("Top Model Insights")
     pretty should include("Top Positive Correlations")
-    pretty should include("Top Negative Correlations")
     pretty should include("Top Contributions")
   }
 
--- readers/src/test/scala/com/salesforce/op/readers/DataGenerationTest.scala
+++ readers/src/test/scala/com/salesforce/op/readers/DataGenerationTest.scala
@@ -142,7 +142,7 @@ class DataGenerationTest extends FlatSpec with PassengerSparkFixtureTest {
       Row("5", null, 2, List("Female"), 0.0, 67, "", List(1471046100),
         Map("Female" -> "string"), Map("Female" -> 1.0), Map("Female" -> false)),
       Row("6", true, null, null, 0.0, null, null, null, null, null, null),
-      Row("4", null, 50, List("Male"), 0.0, 248, "this is a description stuff stuff",
+      Row("4", null, 50, List("Male"), 0.0, 248, "stuff this is a description stuff",
         List(1471046400, 1471046400, 1471046300), Map("Male" -> "string string string"),
         Map("Male" -> 3.0), Map("Male" -> false))
     )
