76cd1e3fc6f5675824df2169205994ac9727cea4
Matthew Tovbin <tovbinm@users.noreply.github.com>
2018-11-07 15:15:20 -0800

    Refactored splitter tests (#176)

--- core/src/main/scala/com/salesforce/op/stages/impl/tuning/OpTrainValidationSplit.scala
+++ core/src/main/scala/com/salesforce/op/stages/impl/tuning/OpTrainValidationSplit.scala
@@ -91,16 +91,16 @@ private[op] class OpTrainValidationSplit[M <: Model[_], E <: Estimator[_]]
    * Creates Train Validation Splits For TS
    *
    * @param stratifyCondition condition to do stratify ts
-   * @param dataset dataset to split
-   * @param label name of label in dataset
-   * @param splitter  used to estimate splitter params prior to ts
+   * @param dataset           dataset to split
+   * @param label             name of label in dataset
+   * @param splitter          used to estimate splitter params prior to ts
    * @return Array[(Train, Test)]
    */
   private[op] override def createTrainValidationSplits[T](
     stratifyCondition: Boolean,
     dataset: Dataset[T],
     label: String,
-    splitter: Option[Splitter] = None
+    splitter: Option[Splitter]
   ): Array[(RDD[Row], RDD[Row])] = {
 
     // get param that stores the label column
