7bfe557d9854269d4d18522ec44889432bd4e761
Chaney Lin <chaney.lin@salesforce.com>
2019-03-01 15:04:10 -0800

    moving PR from forked branch for RFF metrics

---
+++ core/src/main/scala/com/salesforce/op/filters/RawFeatureFilterResults.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2017, Salesforce.com, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.salesforce.op.filters
+
+import com.salesforce.op.stages.impl.preparators.CorrelationType
+
+/**
+ * Contains configuration and results from RawFeatureFilter
+ *
+ * @param rawFeatureFilterConfig  configuration settings for RawFeatureFilter
+ * @param featureDistributions    feature distributions calculated from training data
+ * @param rawFeatureFilterMetrics feature metrics calculated by RawFeatureFilter
+ * @param exclusionReasons        results of RawFeatureFilter tests (reasons why feature is dropped or not)
+ */
+case class RawFeatureFilterResults
+(
+  rawFeatureFilterConfig: RawFeatureFilterConfig = RawFeatureFilterConfig(),
+  featureDistributions: Seq[FeatureDistribution] = Seq.empty,
+  rawFeatureFilterMetrics: Seq[RawFeatureFilterMetrics] = Seq.empty,
+  exclusionReasons: Seq[ExclusionReasons] = Seq.empty
+)
+
+/**
+ * Contains configuration settings for RawFeatureFilter
+ */
+case class RawFeatureFilterConfig
+(
+  minFill: Double = 0.0,
+  maxFillDifference: Double = Double.PositiveInfinity,
+  maxFillRatioDiff: Double = Double.PositiveInfinity,
+  maxJSDivergence: Double = 1.0,
+  maxCorrelation: Double = 1.0,
+  correlationType: CorrelationType = CorrelationType.Pearson,
+  jsDivergenceProtectedFeatures: Set[String] = Set.empty,
+  protectedFeatures: Set[String] = Set.empty
+)
+
+/**
+ * Contains raw feature metrics computing in Raw Feature Filter
+ *
+ * @param name                          feature name
+ * @param trainingFillRate              proportion of values that are null in the training distribution
+ * @param trainingNullLabelAbsoluteCorr correlation between null indicator and the label in the training distribution
+ * @param scoringFillRate               proportion of values that are null in the scoring distribution
+ * @param jsDivergence                  Jensen-Shannon (JS) divergence between the training and scoring distributions
+ * @param fillRateDiff                  absolute difference in fill rates between the training and scoring distributions
+ * @param fillRatioDiff                 ratio of difference in fill rates between the training and scoring distributions
+ */
+case class RawFeatureFilterMetrics
+(
+  name: String,
+  trainingFillRate: Double,
+  trainingNullLabelAbsoluteCorr: Option[Double],
+  scoringFillRate: Option[Double],
+  jsDivergence: Option[Double],
+  fillRateDiff: Option[Double],
+  fillRatioDiff: Option[Double]
+)
+
+/**
+ * Contains results of Raw Feature Filter tests for a given feature
+ *
+ * @param name                    feature name
+ * @param trainingUnfilledState   training fill rate did not meet min required
+ * @param trainingNullLabelLeaker null indicator correlation (absolute) exceeded max allowed
+ * @param scoringUnfilledState    scoring fill rate did not meet min required
+ * @param jsDivergenceMismatch    distribution mismatch: JS Divergence exceeded max allowed
+ * @param fillRateDiffMismatch    distribution mismatch: fill rate difference exceeded max allowed
+ * @param fillRatioDiffMismatch   distribution mismatch: fill ratio difference exceeded max allowed
+ * @param excluded                feature excluded after failing one or more tests
+ */
+case class ExclusionReasons
+(
+  name: String,
+  trainingUnfilledState: Boolean,
+  trainingNullLabelLeaker: Boolean,
+  scoringUnfilledState: Boolean,
+  jsDivergenceMismatch: Boolean,
+  fillRateDiffMismatch: Boolean,
+  fillRatioDiffMismatch: Boolean,
+  excluded: Boolean
+)
