2604daf70c8b18ccff4b5640438187928b569620
Christopher Suchanek <cris.suchanek@gmail.com>
2019-04-16 09:35:25 -0700

    Created new tests for OpCOllection*

---
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OPSetTransformerTest.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017, Salesforce.com, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.salesforce.op.stages.impl.feature
+
+
+import com.salesforce.op.features.types.{Email, EmailMap, Integral, IntegralMap, Real, _}
+import com.salesforce.op.stages.base.unary.{UnaryLambdaTransformer, UnaryTransformer}
+import com.salesforce.op.features.types._
+import com.salesforce.op.stages.base.unary.UnaryLambdaTransformer
+import com.salesforce.op.test.{OpTransformerSpec, TestFeatureBuilder, TestSparkContext}
+import com.salesforce.op.utils.spark.RichDataset._
+import org.apache.spark.sql.Dataset
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import OPSetTransformerTest._
+/**
+ * @author ksuchanek
+ * @since 214
+ */
+@RunWith(classOf[JUnitRunner])
+class OPSetTransformerTest extends OpTransformerSpec[MultiPickList, TransformerType] {
+  lazy val (dataEmailMap, top) = TestFeatureBuilder("name",
+   Seq(MultiPickList(Set("A","B")))
+  )
+
+  /**
+   * [[OpTransformer]] instance to be tested
+   */
+  override val transformer: TransformerType = new TransformerType(
+    transformer = new BaseTransformer(),
+    operationName = "testUnaryMapWrap").setInput(top)
+
+  /**
+   * Input Dataset to transform
+   */
+  override val inputData: Dataset[_] = dataEmailMap
+  /**
+   * Expected result of the transformer applied on the Input Dataset
+   */
+  override val expectedResult: Seq[MultiPickList] = Seq(
+    MultiPickList(Set("a","b"))
+  )
+}
+
+object OPSetTransformerTest {
+  type TransformerType = OPSetTransformer[Text, Text, MultiPickList, MultiPickList]
+
+  class BaseTransformer extends UnaryTransformer[Text, Text](
+    operationName = "testUnary",
+    uid = "1234"
+  ) {
+    override def transformFn: (Text => Text) = (input: Text) => input.value.map(_.toLowerCase()).toText
+  }
+
+}
+
+
+
