038a0b0afd16ea3db1bfeaf78d3f6b911c0f9a82
Chris Rupley <crupley@salesforce.com>
2019-04-17 12:31:05 -0700

    opindextostringnofiltertest extends optransformerspec

--- core/src/test/scala/com/salesforce/op/stages/impl/feature/OpIndexToStringNoFilterTest.scala
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OpIndexToStringNoFilterTest.scala
@@ -32,50 +32,51 @@ package com.salesforce.op.stages.impl.feature
 
 import com.salesforce.op._
 import com.salesforce.op.features.types._
-import com.salesforce.op.test.{TestFeatureBuilder, TestSparkContext}
+import com.salesforce.op.test.{OpTransformerSpec, TestFeatureBuilder}
 import com.salesforce.op.utils.spark.RichDataset._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{Assertions, FlatSpec, Matchers}
 
 
 @RunWith(classOf[JUnitRunner])
-class OpIndexToStringNoFilterTest extends FlatSpec with TestSparkContext {
+class OpIndexToStringNoFilterTest extends OpTransformerSpec[Text, OpIndexToStringNoFilter] {
 
-  val (ds, indF) = TestFeatureBuilder(Seq(0.0, 2.0, 1.0, 0.0, 0.0, 1.0).map(_.toRealNN))
+  val (inputData, indF) = TestFeatureBuilder(Seq(0.0, 2.0, 1.0, 0.0, 0.0, 1.0).map(_.toRealNN))
   val labels = Array("a", "c", "b")
+  override val transformer: OpIndexToStringNoFilter = new OpIndexToStringNoFilter().setInput(indF).setLabels(labels)
   val expected = Array("a", "b", "c", "a", "a", "c").map(_.toText)
+  override val expectedResult: Seq[Text] = Array("a", "b", "c", "a", "a", "c").map(_.toText)
 
   val labelsNew = Array("a", "c")
   val expectedNew = Array("a", OpIndexToStringNoFilter.unseenDefault, "c", "a", "a", "c").map(_.toText)
 
   Spec[OpIndexToStringNoFilter] should "correctly deindex a numeric column" in {
     val indexToStr = new OpIndexToStringNoFilter().setInput(indF).setLabels(labels)
-    val strs = indexToStr.transform(ds).collect(indexToStr.getOutput())
+    val strs = indexToStr.transform(inputData).collect(indexToStr.getOutput())
 
     strs shouldBe expected
   }
 
   it should "correctly deindex a numeric column (shortcut)" in {
     val str = indF.deindexed(labels)
-    val strs = str.originStage.asInstanceOf[OpIndexToStringNoFilter].transform(ds).collect(str)
+    val strs = str.originStage.asInstanceOf[OpIndexToStringNoFilter].transform(inputData).collect(str)
     strs shouldBe expected
 
     val str2 = indF.deindexed(labels, handleInvalid = IndexToStringHandleInvalid.Error)
-    val strs2 = str2.originStage.asInstanceOf[OpIndexToString].transform(ds).collect(str2)
+    val strs2 = str2.originStage.asInstanceOf[OpIndexToString].transform(inputData).collect(str2)
     strs2 shouldBe expected
   }
 
   it should "correctly deindex even if the lables list does not match the number of indicies" in {
     val indexToStr = new OpIndexToStringNoFilter().setInput(indF).setLabels(labelsNew)
-    val strs = indexToStr.transform(ds).collect(indexToStr.getOutput())
+    val strs = indexToStr.transform(inputData).collect(indexToStr.getOutput())
 
     strs shouldBe expectedNew
   }
 
   Spec[OpIndexToString] should "correctly deindex a numeric column" in {
     val indexToStr = new OpIndexToString().setInput(indF).setLabels(labels)
-    val strs = indexToStr.transform(ds).collect(indexToStr.getOutput())
+    val strs = indexToStr.transform(inputData).collect(indexToStr.getOutput())
 
     strs shouldBe expected
   }
