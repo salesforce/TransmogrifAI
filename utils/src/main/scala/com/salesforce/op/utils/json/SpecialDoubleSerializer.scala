12f533395385a6aaab37344a2a2e3e6168281415
Matthew Tovbin <mtovbin@salesforce.com>
2019-05-30 14:09:07 -0700

    special double serializer fix

--- utils/src/main/scala/com/salesforce/op/utils/json/SpecialDoubleSerializer.scala
+++ utils/src/main/scala/com/salesforce/op/utils/json/SpecialDoubleSerializer.scala
@@ -31,7 +31,7 @@
 package com.salesforce.op.utils.json
 
 import org.json4s.CustomSerializer
-import org.json4s.JsonAST.JString
+import org.json4s.JsonAST.{JDouble, JString}
 
 /**
  * Json4s serializer for marshalling special Double values: NaN, -Infinity and Infinity
@@ -42,6 +42,7 @@ class SpecialDoubleSerializer extends CustomSerializer[Double](_ =>
     case JString("NaN") => Double.NaN
     case JString("-Infinity") => Double.NegativeInfinity
     case JString("Infinity") => Double.PositiveInfinity
+    case JDouble(v) => v
   }, {
     case v: Double if v.isNaN => JString("NaN")
     case Double.NegativeInfinity => JString("-Infinity")
