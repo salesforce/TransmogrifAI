ac9e0cc55077d1c39e97835a690d909927f4c684
mweilsalesforce <mweil@salesforce.com>
2019-06-21 14:07:09 -0700

    Changing TypeName Again

--- readers/src/main/scala/com/salesforce/op/readers/Reader.scala
+++ readers/src/main/scala/com/salesforce/op/readers/Reader.scala
@@ -51,7 +51,7 @@ private[readers] trait ReaderType[T] extends Serializable {
    *
    * @return full input type name
    */
-  final def fullTypeName: String = wtt.tpe.typeSymbol.fullName
+  final def fullTypeName: String = wtt.tpe.toString
 
   /**
    * Short reader input type name
@@ -66,8 +66,7 @@ private[readers] trait ReaderType[T] extends Serializable {
    * @param opParams contains map of reader type to ReaderParams instances
    * @return ReaderParams instance if it exists
    */
-  final def getReaderParams(opParams: OpParams): Option[ReaderParams] = opParams.readerParams
-    .get(this.wtt.tpe.toString.split("\\.").last)
+  final def getReaderParams(opParams: OpParams): Option[ReaderParams] = opParams.readerParams.get(typeName)
 
 }
 
