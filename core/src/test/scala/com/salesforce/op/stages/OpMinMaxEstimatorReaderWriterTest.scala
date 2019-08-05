dccd6030d9041737720f2731378934c7cbe54d9b
Erica Chiu <erica.chiu@salesforce.com>
2019-08-05 15:23:27 -0700

    Add descaling test to MinMaxNormEstimator

--- core/src/test/scala/com/salesforce/op/stages/OpMinMaxEstimatorReaderWriterTest.scala
+++ core/src/test/scala/com/salesforce/op/stages/OpMinMaxEstimatorReaderWriterTest.scala
@@ -33,10 +33,13 @@ package com.salesforce.op.stages
 import com.salesforce.op._
 import com.salesforce.op.features.types._
 import com.salesforce.op.stages.base.unary.{UnaryEstimator, UnaryModel}
-import com.salesforce.op.stages.impl.feature.{LinearScalerArgs, ScalerMetadata, ScalingType}
+import com.salesforce.op.stages.impl.feature.{DescalerTransformer, LinearScalerArgs, ScalerMetadata, ScalingType, StandardMinEstimator}
+import com.salesforce.op.test.TestFeatureBuilder
 import org.apache.spark.sql.Dataset
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+
+import scala.util.{Failure, Success}
 
 
 @RunWith(classOf[JUnitRunner])
@@ -47,6 +50,42 @@ class OpMinMaxEstimatorReaderWriterTest extends OpPipelineStageReaderWriterTest 
 
   val expected =
     Array(1.0.toReal, 0.0.toReal, Real.empty, 0.10476190476190476.toReal, 0.2761904761904762.toReal, 0.0.toReal)
+
+  val inputValues: Seq[Double] = Seq(10, 100, 1000)
+
+  val (inputData, testF) = TestFeatureBuilder(inputValues.map(_.toReal))
+
+  it should "descale and work in min-max normalized workflow" in {
+    val featureNormalizer = new MinMaxNormEstimator().setInput(testF)
+    val normedOutput = featureNormalizer.getOutput()
+    val metadata = featureNormalizer.fit(inputData).getMetadata()
+
+    val expectedMin = inputValues.min
+    val expectedMax = inputValues.max
+    val expectedSlope = 1 / (expectedMax - expectedMin)
+    val expectedIntercept = - expectedMin / (expectedMax - expectedMin)
+    ScalerMetadata(metadata) match {
+      case Failure(err) => fail(err)
+      case Success(meta) =>
+        meta shouldBe a[ScalerMetadata]
+        meta.scalingType shouldBe ScalingType.Linear
+        meta.scalingArgs shouldBe a[LinearScalerArgs]
+        math.abs((meta.scalingArgs.asInstanceOf[LinearScalerArgs].slope - expectedSlope)
+          / expectedSlope) should be < 0.001
+        math.abs((meta.scalingArgs.asInstanceOf[LinearScalerArgs].intercept - expectedIntercept)
+          / expectedIntercept) should be < 0.001
+    }
+
+    val descaledResponse = new DescalerTransformer[Real, Real, Real]()
+      .setInput(normedOutput, normedOutput).getOutput()
+    val workflow = new OpWorkflow().setResultFeatures(descaledResponse)
+    val wfModel = workflow.setInputDataset(inputData).train()
+    val transformed = wfModel.score()
+
+    val actual = transformed.collect().map(_.getAs[Double](1))
+    val expected : Seq[Double] = inputValues
+    all(actual.zip(expected).map(x => math.abs(x._2 - x._1))) should be < 0.0001
+  }
 }
 
 
