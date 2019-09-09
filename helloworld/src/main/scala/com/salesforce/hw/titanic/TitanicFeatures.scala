3a1316aa3ecf0ed47d81ddc6711002ecab75c9b2
Gera Shegalov <gshegalov@salesforce.com>
2019-09-09 00:09:59 -0700

    Titanic lambdas

--- helloworld/src/main/scala/com/salesforce/hw/titanic/TitanicFeatures.scala
+++ helloworld/src/main/scala/com/salesforce/hw/titanic/TitanicFeatures.scala
@@ -30,19 +30,68 @@
 
 package com.salesforce.hw.titanic
 
+import java.io.Serializable
+
+import com.salesforce.hw.titanic.TitanicFeatures._
 import com.salesforce.op.features.FeatureBuilder
 import com.salesforce.op.features.types._
 
 trait TitanicFeatures extends Serializable {
-  val survived = FeatureBuilder.RealNN[Passenger].extract(_.getSurvived.toDouble.toRealNN).asResponse
-  val pClass = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getPclass).map(_.toString).toPickList).asPredictor // scalastyle:off
-  val name = FeatureBuilder.Text[Passenger].extract(d => Option(d.getName).toText).asPredictor
-  val sex = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getSex).toPickList).asPredictor
-  val age = FeatureBuilder.Real[Passenger].extract(d => Option(Double.unbox(d.getAge)).toReal).asPredictor
-  val sibSp = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getSibSp).map(_.toString).toPickList).asPredictor
-  val parch = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getParch).map(_.toString).toPickList).asPredictor
-  val ticket = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getTicket).toPickList).asPredictor
-  val fare = FeatureBuilder.Real[Passenger].extract(d => Option(Double.unbox(d.getFare)).toReal).asPredictor
-  val cabin = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getCabin).toPickList).asPredictor
-  val embarked = FeatureBuilder.PickList[Passenger].extract(d => Option(d.getEmbarked).toPickList).asPredictor
+  val survived = FeatureBuilder.RealNN[Passenger].extract(new SurvivedFunc).asResponse
+  val pClass = FeatureBuilder.PickList[Passenger].extract(new pClassFunc).asPredictor // scalastyle:off
+  val name = FeatureBuilder.Text[Passenger].extract(new NameFunc).asPredictor
+  val sex = FeatureBuilder.PickList[Passenger].extract(new SexFunc).asPredictor
+  val age = FeatureBuilder.Real[Passenger].extract(new AgeFunc).asPredictor
+  val sibSp = FeatureBuilder.PickList[Passenger].extract(new SibSpFunc).asPredictor
+  val parch = FeatureBuilder.PickList[Passenger].extract(new ParchFunc).asPredictor
+  val ticket = FeatureBuilder.PickList[Passenger].extract(new TicketFunc).asPredictor
+  val fare = FeatureBuilder.Real[Passenger].extract(new FareFunc).asPredictor
+  val cabin = FeatureBuilder.PickList[Passenger].extract(new CabinFunc).asPredictor
+  val embarked = FeatureBuilder.PickList[Passenger].extract(new EmbarkedFunc).asPredictor
+}
+
+object TitanicFeatures {
+  class SurvivedFunc extends Function1[Passenger, RealNN] with Serializable {
+    override def apply(v1: Passenger): RealNN = v1.getSurvived.toDouble.toRealNN
+  }
+
+  class pClassFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getPclass).map(_.toString).toPickList
+  }
+
+  class NameFunc extends Function1[Passenger, Text] with Serializable {
+    override def apply(v1: Passenger): Text = Option(v1.getName).toText
+  }
+
+  class SexFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getSex).toPickList
+  }
+
+  class AgeFunc extends Function1[Passenger, Real] with Serializable {
+    override def apply(v1: Passenger): Real = Option(Double.unbox(v1.getAge)).toReal
+  }
+
+  class SibSpFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getSibSp).map(_.toString).toPickList
+  }
+
+  class ParchFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getParch).map(_.toString).toPickList
+  }
+
+  class TicketFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getTicket).toPickList
+  }
+
+  class FareFunc extends Function1[Passenger, Real] with Serializable {
+    override def apply(v1: Passenger): Real = Option(Double.unbox(v1.getFare)).toReal
+  }
+
+  class CabinFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getCabin).toPickList
+  }
+
+  class EmbarkedFunc extends Function1[Passenger, PickList] with Serializable {
+    override def apply(v1: Passenger): PickList = Option(v1.getEmbarked).toPickList
+  }
 }
