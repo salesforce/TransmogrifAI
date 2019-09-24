025ff6fdf3ecb138071ddccbec95d3d8aa54e01c
Tuan Nguyen <anhtuan277@gmail.com>
2019-09-24 15:03:30 -0700

    implement base outlier detector

---
+++ core/src/main/scala/com/salesforce/op/stages/impl/feature/OutlierDetector.scala
@@ -0,0 +1,29 @@
+package com.salesforce.op.stages.impl.feature
+
+import com.salesforce.op.UID
+import com.salesforce.op.features.types.{Binary, RealNN}
+import com.salesforce.op.stages.base.unary.{UnaryEstimator, UnaryModel}
+import org.apache.spark.sql.{DataFrame, Dataset}
+
+/**
+ * @author nguyen.tuan
+ * @since 214
+ */
+class OutlierDetector(uid: String = UID[OutlierDetector], operationName: String = "outlier detector")
+  extends UnaryEstimator[RealNN, Binary](uid, operationName) {
+  override def fitFn(dataset: Dataset[Option[Double]]): UnaryModel[RealNN, Binary] = {
+    new OutlierDetectorModel(operationName = operationName, uid = uid)
+
+  }
+
+
+}
+
+class OutlierDetectorModel(operationName: String, uid: String)
+  extends UnaryModel[RealNN, Binary](uid = uid, operationName = operationName) {
+  override def transform(dataset: Dataset[_]): DataFrame = dataset.toDF()
+
+  // scalastyle:off
+  override def transformFn: RealNN => Binary = ???
+  // scalastyle:on
+}
--- core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
@@ -20,3 +20,4 @@ class OutlierDetectorTest extends FlatSpec with TestSparkContext{
 
   }
 }
+
