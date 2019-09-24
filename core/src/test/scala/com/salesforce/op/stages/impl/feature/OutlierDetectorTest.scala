52ec9ae072fef6c2c1319989225eb857d5369395
mweilsalesforce <mweil@salesforce.com>
2019-09-24 14:36:11 -0700

    Creating test

---
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
@@ -0,0 +1,8 @@
+package com.salesforce.op.stages.impl.feature
+
+import com.salesforce.op.features.types.RealNN
+import com.salesforce.op.test.OpTransformerSpec
+
+class OutlierDetectorTest extends OpTransformerSpec[RealNN, Boolean] {
+
+}
