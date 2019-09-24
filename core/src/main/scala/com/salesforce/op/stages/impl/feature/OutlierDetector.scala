35d230fb1341916759021d45e99d9bf7f8289ebe
mweilsalesforce <mweil@salesforce.com>
2019-09-24 15:42:10 -0700

    4th test done

--- core/src/main/scala/com/salesforce/op/stages/impl/feature/OutlierDetector.scala
+++ core/src/main/scala/com/salesforce/op/stages/impl/feature/OutlierDetector.scala
@@ -1,9 +1,10 @@
 package com.salesforce.op.stages.impl.feature
 
 import com.salesforce.op.UID
-import com.salesforce.op.features.types.{Binary, RealNN}
+import com.salesforce.op.features.types._
 import com.salesforce.op.stages.base.unary.{UnaryEstimator, UnaryModel}
-import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.functions._
 
 /**
  * @author nguyen.tuan
@@ -12,18 +13,19 @@ import org.apache.spark.sql.{DataFrame, Dataset}
 class OutlierDetector(uid: String = UID[OutlierDetector], operationName: String = "outlier detector")
   extends UnaryEstimator[RealNN, Binary](uid = uid, operationName = operationName) {
   override def fitFn(dataset: Dataset[Option[Double]]): UnaryModel[RealNN, Binary] = {
-    new OutlierDetectorModel(operationName = operationName, uid = uid)
+    val minElement = dataset.agg(min(col("value"))).first().toSeq.head.asInstanceOf[Double]
+    new OutlierDetectorModel(operationName = operationName, uid = uid, minElement = minElement)
 
   }
 
 
 }
 
-class OutlierDetectorModel(operationName: String, uid: String)
+class OutlierDetectorModel(operationName: String, uid: String, minElement: Double)
   extends UnaryModel[RealNN, Binary](uid = uid, operationName = operationName) {
-  override def transform(dataset: Dataset[_]): DataFrame = dataset.toDF()
+  // override def transform(dataset: Dataset[_]): DataFrame = dataset.toDF()
 
   // scalastyle:off
-  override def transformFn: RealNN => Binary = ???
+  override def transformFn: RealNN => Binary = (value: RealNN) => (value.value.get > minElement).toBinary
   // scalastyle:on
 }
--- core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
@@ -5,23 +5,45 @@ import com.salesforce.op.stages.base.unary.UnaryEstimator
 import com.salesforce.op.test._
 import org.apache.spark.sql.Row
 import org.scalatest.FlatSpec
+import com.salesforce.op.utils.spark.RichDataset._
 
 
 class OutlierDetectorTest extends FlatSpec with TestSparkContext{
 
   val detector: UnaryEstimator[RealNN, Binary] = new OutlierDetector()
+  lazy val output = detector.getOutput()
   it should "return an empty dataset when actually checking an empty dataset" in {
     val emptySeq = Seq.empty[Double].map(_.toRealNN)
     val (inputData, f1) = TestFeatureBuilder(emptySeq)
-    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect()
-    result shouldBe Array.empty[Row]
+    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect(output)
+    println(result.toSeq)
+    result shouldBe Array.empty[Boolean]
 
   }
   it should "return the same dataset when the data contains only one element" in {
     val oneElementSeq = Seq(42.0).map(_.toRealNN)
     val (inputData, f1) = TestFeatureBuilder(oneElementSeq)
-    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect()
-    result shouldBe Array(Row(42.0))
+    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect(output)
+    result shouldBe Array(false.toBinary)
+
+  }
+
+  it should "not detect any outlier when the data contains the same repeating element" in {
+    val multipleElementSeq = Seq.fill(100)(42.0).map(_.toRealNN)
+    val (inputData, f1) = TestFeatureBuilder(multipleElementSeq)
+    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect(output)
+    result shouldBe Array.fill(100)(false.toBinary)
+
+  }
+
+
+  it should "detect an outlier if all the elements are the same but one is much larger" in {
+    val element = 42.0
+    val multipleElementSeq = Seq.fill(100)(element).map(_.toRealNN)
+    val seqWithOutlier = multipleElementSeq :+ ((element * 1e9).toRealNN)
+    val (inputData, f1) = TestFeatureBuilder(seqWithOutlier)
+    val result = detector.setInput(f1).fit(inputData).transform(inputData).collect(output)
+    result shouldBe Array.fill(100)(false.toBinary) :+ true.toBinary
 
   }
 }
