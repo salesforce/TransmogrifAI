75ff42f751e4eb6c2d356b938e4c6b20bdb84071
mweilsalesforce <mweil@salesforce.com>
2019-09-24 14:45:37 -0700

    First Test

--- core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
+++ core/src/test/scala/com/salesforce/op/stages/impl/feature/OutlierDetectorTest.scala
@@ -1,8 +1,22 @@
 package com.salesforce.op.stages.impl.feature
 
-import com.salesforce.op.features.types.RealNN
-import com.salesforce.op.test.OpTransformerSpec
+import com.salesforce.op.features.types._
+import com.salesforce.op.stages.base.unary.UnaryEstimator
+import com.salesforce.op.test._
+import org.apache.spark.sql.Row
+import org.scalatest.FlatSpec
 
-class OutlierDetectorTest extends OpTransformerSpec[RealNN, Boolean] {
 
+class OutlierDetectorTest extends FlatSpec with TestSparkContext{
+
+  val detector: UnaryEstimator[RealNN, Binary] = new OutlierDetector()
+  it should "return an empty dataset when actually checking an empty dataset" in {
+
+    import spark.sqlContext.implicits._
+    val data = spark.sparkContext.parallelize(Seq.empty[Double]).toDS()
+    val result = detector.fit(data).transform(data).collect()
+
+    result shouldBe Array.empty[Row]
+
+  }
 }
