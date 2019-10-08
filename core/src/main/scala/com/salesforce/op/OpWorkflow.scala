2a4ccbc2c688cfa5d5d147e43ab6a93e07cffdb2
Tuan Nguyen <anhtuan277@gmail.com>
2019-10-08 13:05:51 -0700

    Update OpWorkflow.scala

--- core/src/main/scala/com/salesforce/op/OpWorkflow.scala
+++ core/src/main/scala/com/salesforce/op/OpWorkflow.scala
@@ -559,7 +559,7 @@ class OpWorkflow(val uid: String = UID[OpWorkflow]) extends OpWorkflowCore {
         textBinsFormula = textBinsFormula,
         timePeriod = timePeriod,
         minScoringRows = minScoringRows,
-        minUniqueTokenLen = minTok)
+        minUniqueTokenLen = minUniqueTokenLen)
     }
     this
   }
