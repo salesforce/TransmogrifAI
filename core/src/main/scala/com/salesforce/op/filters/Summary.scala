746c9fb614f13b3a8ee9dc1914a1702da228e772
Tuan Nguyen <anhtuan277@gmail.com>
2020-01-31 16:21:41 -0800

    Update Summary.scala

--- core/src/main/scala/com/salesforce/op/filters/Summary.scala
+++ core/src/main/scala/com/salesforce/op/filters/Summary.scala
@@ -59,7 +59,9 @@ case object Summary {
    */
   def apply(preppedFeature: ProcessedSeq): Summary = {
     preppedFeature match {
-      case Left(v) => Summary(v.size, v.size, v.size, 1.0, v.map(d => hllMonoid.create(d.getBytes)).reduce(_ + _))
+      case Left(v) => Summary(v.size, v.size, v.size, 1.0, v.map(
+        d => hllMonoid.create(d.getBytes)).reduceOption(_ + _).getOrElse(hllMonoid.zero)
+      )
       case Right(v) => monoid.sum(v.map(d => Summary(d, d, d, 1.0, hllMonoid.create(d.toString.getBytes))))
     }
   }
