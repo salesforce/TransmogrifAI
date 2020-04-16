dc33adab195630f4a9b8ad33dcab97a7c3e88560
Tuan Nguyen <anhtuan277@gmail.com>
2020-04-16 11:44:59 -0700

    Fix Days.daysBetween int overflow (#471)

--- core/src/main/scala/com/salesforce/op/stages/impl/feature/DateListVectorizer.scala
+++ core/src/main/scala/com/salesforce/op/stages/impl/feature/DateListVectorizer.scala
@@ -41,7 +41,7 @@ import com.salesforce.op.utils.spark.OpVectorColumnMetadata
 import enumeratum._
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.param.{BooleanParam, DoubleParam, LongParam, ParamValidators}
-import org.joda.time.{DateTime, DateTimeConstants, Days}
+import org.joda.time.{DateTime, DateTimeConstants}
 
 import scala.reflect.runtime.universe._
 
@@ -203,7 +203,7 @@ class DateListVectorizer[T <: OPList[Long]]
       if (dt.isEmpty) Seq($(fillValue))
       else {
         val compareDate = if ($(first)) dt.v.min else dt.v.max
-        Seq(Days.daysBetween(new DateTime(compareDate, DateTimeUtils.DefaultTimeZone), getReferenceDate()).getDays)
+        Seq(DateTimeUtils.getStandardDays(startMillis = compareDate, endMillis = $(referenceDate)))
       }
     if ($(trackNulls)) days :+ (dt.isEmpty : Double) else days
   }
