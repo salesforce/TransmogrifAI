103e15a9b0eba9c3f160c5725a5bb25f6d9cc805
Tuan Nguyen <anhtuan277@gmail.com>
2020-05-07 08:35:47 -0700

    Update Transmogrifier.scala

--- core/src/main/scala/com/salesforce/op/stages/impl/feature/Transmogrifier.scala
+++ core/src/main/scala/com/salesforce/op/stages/impl/feature/Transmogrifier.scala
@@ -53,7 +53,7 @@ private[op] trait TransmogrifierDefaults {
   val NullString: String = OpVectorColumnMetadata.NullString
   val OtherString: String = OpVectorColumnMetadata.OtherString
   val DefaultNumOfFeatures: Int = 512
-  val MaxNumOfFeatures: Int = 200000
+  val MaxNumOfFeatures: Int = 2000000
   val DateListDefault: DateListPivot = DateListPivot.SinceLast
   val ReferenceDate: org.joda.time.DateTime = DateTimeUtils.now()
   val TopK: Int = 20
